9f05491b85f98c92331c03292144fa7aac365898
Daniil <nlb.marian@gmail.com>
2026-04-06 17:41:42 +0300

    Написал модель для хранения данных о погоде в определенном городе

--- weatherApp/Models/WeatherRecord.cs
+++ weatherApp/Models/WeatherRecord.cs
@@ -2,5 +2,10 @@
 
 public class WeatherRecord
 {
-    
+    public int Id { get; set; }
+    public string City { get; set; } = string.Empty;
+    public double Temperature { get; set; }
+    public double Humidity { get; set; }
+    public double Pressure { get; set; }
+    public double WindSpeed { get; set; }
 }

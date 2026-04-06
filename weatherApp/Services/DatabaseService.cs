28e0c5856aea0f0e49cdf55bdd9466f3b6745413
Daniil <nlb.marian@gmail.com>
2026-04-06 18:04:01 +0300

    Написал логику DatabaseService.cs

--- weatherApp/Services/DatabaseService.cs
+++ weatherApp/Services/DatabaseService.cs
@@ -1,8 +1,66 @@
 ﻿using System.IO;
+using Microsoft.Data.Sqlite;
 
 namespace weatherApp.Services;
 
 public class DatabaseService
 {
+    private readonly string _connectionString;
+
+
+    public DatabaseService(string databaseName = "weather.db")
+    {
+        string dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, databaseName);
+  
+        _connectionString = $"Data Source={dbPath}";
+    }
     
+    public void InitializeDatabase()
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        
+        connection.Open();
+        
+        CreateSettingsTable(connection);
+        CreateWeatherRecordsTable(connection);
+        InsertInitialData(connection);
+    }
+
+    public void CreateSettingsTable(SqliteConnection connection)
+    {
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Settings (
+                id INTEGER PRIMARY KEY CHECK (id = 1),
+                theme TEXT NOT NULL DEFAULT 'light',
+                lastCity TEXT NOT NULL DEFAULT 'Москва'
+            )
+        ";
+        
+        command.ExecuteNonQuery();
+    }
+    
+    public void CreateWeatherRecordsTable(SqliteConnection connection)
+    {
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS FavoriteCities (
+                id INTEGER  PRIMARY KEY AUTOINCREMENT,
+                city TEXT NOT NULL UNIQUE
+            )
+        ";
+        
+        command.ExecuteNonQuery();
+    }
+
+    public void InsertInitialData(SqliteConnection connection)
+    {
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            INSERT OR IGNORE  INTO Settings (id, theme, lastCity)
+            VALUES (1, 'light', 'Москва')
+        ";
+        
+        command.ExecuteNonQuery();
+    }
 }

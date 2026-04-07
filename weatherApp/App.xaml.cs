795bb240002e56081f3792e921b9554cb3de3e96
Daniil <nlb.marian@gmail.com>
2026-04-07 16:10:32 +0300

    Добавил создание GeocodingService, WeatherService и WeatherRepository в App.xaml.cs, настроил внедрение зависимостей в MainWindowViewModel

--- weatherApp/App.xaml.cs
+++ weatherApp/App.xaml.cs
@@ -1,19 +1,42 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using DotNetEnv;
 using weatherApp.Services;
+using weatherApp.ViewModels;
 
 namespace weatherApp;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
+        
+        Env.Load();
+        
         var dbService = new DatabaseService();
         dbService.InitializeDatabase();
+        
+        string connectionString = dbService.GetConnectionString();
+
+        var repository = new WeatherRepository(connectionString);
+        
+        string geoApiKey = Env.GetString("GEOCODE_API_KEY");
+        string weatherApiKey = Env.GetString("WEATHER_API_KEY");
+
+        var geocodingService = new GeocodingService(geoApiKey);
+        var weatherService = new WeatherService(weatherApiKey);
+        
+        var viewModel = new MainWindowViewModel(
+            geocodingService,
+            weatherService,
+            repository
+        );
+        
+        var mainWindow = new MainWindow
+        {
+            DataContext = viewModel
+        };
+        
+        mainWindow.Show();
     }
 }
--- weatherApp/ViewModels/MainWindowViewModel.cs
+++ weatherApp/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace weatherApp.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DotNetEnv;
+using weatherApp.Services;
 
-public class MainWindowViewModel
+namespace weatherApp.ViewModels;
+
+public partial class MainWindowViewModel
 {
+    private readonly GeocodingService _geocodingService;
+    private readonly WeatherService _weatherService;
+    private readonly WeatherRepository _repository;
     
+    public MainWindowViewModel(
+        GeocodingService geocodingService,
+        WeatherService weatherService,
+        WeatherRepository repository)
+    {
+        _geocodingService = geocodingService;
+        _weatherService = weatherService;
+        _repository = repository;
+    }
 }

d3489149c6420693abd8bbd22bfb48e96cb89260
Daniil <nlb.marian@gmail.com>
2026-04-07 12:22:41 +0300

    Добавил модель GeoapifyResponse

---
+++ weatherApp/Models/GeoapifyResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace weatherApp.Models;
+
+public class GeoapifyResponse
+{
+    [JsonPropertyName("features")]
+    public List<Feature> Features { get; set; } = new();
+    
+    public class Feature
+    {
+        [JsonPropertyName("properties")]
+        public Coordinates Properties { get; set; } = new();
+    }
+    
+    public class Coordinates
+    {
+        [JsonPropertyName("lon")]
+        public double Longitude { get; set; }
+        
+        [JsonPropertyName("lat")]
+        public double Latitude { get; set; }
+    }
+}

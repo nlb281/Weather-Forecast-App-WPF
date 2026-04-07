68956d46598b4fec99d81104e8832cd58aee1b17
Daniil <nlb.marian@gmail.com>
2026-04-07 15:28:19 +0300

    Написал запрос на получение погоды по координатам

---
+++ weatherApp/Services/WeatherService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using weatherApp.Models;
+
+namespace weatherApp.Services;
+
+public class WeatherService
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+    private const string BaseUrl = "https://api.weather.yandex.ru/v2/";
+
+    public WeatherService(string apiKey)
+    {
+        _httpClient = new HttpClient();
+        _apiKey = apiKey;
+        
+        _httpClient.DefaultRequestHeaders.Add("X-Yandex-Weather-Key", _apiKey);
+    }
+    
+    public async Task<WeatherService?> GetCoords(double latitude, double longitude)
+    {
+        try
+        {
+            string url = $"{BaseUrl}/forecast?lat={latitude}&lon={longitude}&lang=ru_RU";
+            var res = await _httpClient.GetAsync(url);
+            
+            if (!res.IsSuccessStatusCode)
+                return null;
+            
+            string json = await res.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<WeatherService>(json);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка получения данных погоды: {ex.Message}");
+            return null;
+        }
+    }
+}

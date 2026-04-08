f0a3c6ceca72a2da6583907a61fa9354cffaa3bf
Daniil <nlb.marian@gmail.com>
2026-04-08 17:33:50 +0300

    Добавил FavoriteCitiesWindow.xaml

---
+++ weatherApp/Views/FavoriteCitiesWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace weatherApp.Views;
+
+public partial class FavoriteCitiesWindow : Window
+{
+    public FavoriteCitiesWindow()
+    {
+        InitializeComponent();
+    }
+}

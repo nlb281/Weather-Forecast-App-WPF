c599f5291416f19957c2da9895dd60760e216954
Daniil <nlb.marian@gmail.com>
2026-04-08 17:32:45 +0300

    Добавил FavoriteCitiesViewModel.cs

---
+++ weatherApp/ViewModels/FavoriteCitiesViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using weatherApp.Services;
+
+namespace weatherApp.ViewModels;
+
+public partial class FavoriteCitiesViewModel : ObservableObject 
+{
+    private readonly WeatherRepository _repository;
+    
+    [ObservableProperty]
+    private ObservableCollection<string> favoriteCities = new();
+    
+    public FavoriteCitiesViewModel(WeatherRepository repository)
+    {
+        _repository = repository;
+        
+        List<string> cities = _repository.GetFavoriteCities();
+        
+        foreach (string city in cities)
+        {
+            FavoriteCities.Add(city);
+        }
+    }
+}
